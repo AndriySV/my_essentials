7e970b4a75bbf01fbae4f0030b44d4fd0fab502d
AndriySV <svyryd.andriy@gmail.com>
2015-05-31 13:04:27 +0300

    my checkbox

---
+++ src/main/java/chapter6/mvc/GridCheckboxController.java
@@ -0,0 +1,10 @@
+package chapter6.mvc;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+
+public class GridCheckboxController extends SelectorComposer<Component> {
+
+	
+	
+}
